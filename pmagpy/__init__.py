69805b9b2e5d6ce02e8e4bae1d1684380a8ba585
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2023-02-25 20:13:27 -0800

    commit ipmag, pmag, pmagplotlib to generate API

---
+++ pmagpy/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+import sys
+
+""".format(sys.version))
+from . import pmag
+from . import ipmag
+from . import pmagplotlib
+
+__all__ = [pmag, ipmag, pmagplotlib]
