4ed659e4a0e7d54d2285102e8d71e84061a6cf17
apivarunas <apiv93@gmail.com>
2025-10-06 10:54:07 -0700

    adding rockmagpy to init

--- pmagpy/__init__.py
+++ pmagpy/__init__.py
@@ -4,5 +4,6 @@ import sys
 from . import pmag
 from . import ipmag
 from . import pmagplotlib
+from . import rockmagpy
 
-__all__ = [pmag, ipmag, pmagplotlib]
+__all__ = [pmag, ipmag, pmagplotlib, rockmagpy]
